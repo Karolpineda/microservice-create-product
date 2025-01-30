80912b96e0956b038501cbefd7a2f7cf281ef657
Karolpineda <karolpineda@hotmail.com>
2025-01-30 18:04:25 -0500

    feat: :construction: Update of api

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./docs/swagger");
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+
+// Enable CORS (with default or custom settings)
+
+// Middleware to proceses JSON
+app.use(express.json());
+
+// Rutes
+app.use("/api", productRoutes);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+module.exports = app;
